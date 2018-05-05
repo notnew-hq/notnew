f8cdd4d6190411e41477b47204affdd584fe8e1c
admhny <adam@adamhaney.co>
2018-05-04 18:46:46 -0700

    Fixed photos

--- _site/assets/js/notnew.js
+++ _site/assets/js/notnew.js
@@ -11,21 +11,21 @@ var allProducts = [
   {
 		title: 'Sweatshirt',
 		desc: 'Black sweatshirt with print on chest and right arm.',
-		img: '/assets/img/2018-03-08-not-new-img_157.jpg',
+		img: '/assets/img/black-sweatchirt.jpg',
 		price: '40',
     sku: 'NNH001'
 	},
 	{
 		title: 'Sweatshirt',
 		desc: 'Blue sweatshirt with yellow front and back print.',
-		img: '/assets/img/2018-03-08-not-new-img_6.jpg',
+		img: '/assets/img/blue-sweatshirt-yellow-print.jpg',
 		price: '40',
     sku: 'NNH002'
 	},
 	{
 		title: 'T-shirt',
 		desc: 'Black shirt with front and back green print.',
-		img: '/assets/img/2018-03-08-not-new-img_3.jpg',
+		img: '/assets/img/black-shirt.jpg',
 		price: '25',
     sku: 'NNH003'
 	},
