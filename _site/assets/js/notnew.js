a5ee867adab92be48620cbd500880c8171aa0ee6
admhny <adam@adamhaney.co>
2018-10-01 19:31:47 -0700

    Added Blue Sweatshirt

--- _site/assets/js/notnew.js
+++ _site/assets/js/notnew.js
@@ -9,8 +9,20 @@ function isTouch() { // check to see if touch screen
 }
 var allProducts = [
 	{
+		title: 'Blue Sweatshirt',
+		desc: 'Blue sweatshirt with white and black print (Made to order)',
+		images: [
+			'/assets/img/not-new-blue-0003.jpg',
+			'/assets/img/not-new-blue-0004.jpg',
+			'/assets/img/not-new-blue-0002.jpg',
+			'/assets/img/not-new-blue-0001.jpg',
+		],
+		price: '45',
+		sku: 'NNH008'
+	},
+	{
 		title: 'White T-Shirt',
-		desc: 'White t-shirt with front and back black print.',
+		desc: 'White t-shirt with front and back black print',
 		images: [
 			'/assets/img/not-new-white-t-5.jpg',
 			'/assets/img/not-new-white-t-10.jpg',
@@ -18,11 +30,23 @@ var allProducts = [
 			'/assets/img/not-new-white-t-1.jpg',
 		],
 		price: '21',
+		sku: 'NNH004'
+	},
+	{
+		title: 'White T-Shirt',
+		desc: 'White t-shirt two-toned',
+		images: [
+			'/assets/img/not-new-two-colors-white-t-1.jpg',
+			'/assets/img/not-new-two-colors-white-t-2.jpg',
+			'/assets/img/not-new-two-colors-white-t-3.jpg',
+			'/assets/img/not-new-two-colors-white-t-4.jpg',
+		],
+		price: '29',
 		sku: 'NNH007'
 	},
 	{
 		title: 'Pink Sweatshirt',
-		desc: 'Pink sweatshirt with white print. (Pink L and XL on backorder.)',
+		desc: 'Pink sweatshirt with white print. (Pink L and XL on backorder)',
 		images: [
 			'/assets/img/not-new-pink-hoody-w-1.JPG',
 			'/assets/img/not-new-pink-hoody-w-2.JPG',
@@ -34,7 +58,7 @@ var allProducts = [
 	},
 	{
 		title: 'Sand Sweatshirt',
-		desc: 'Sand sweatshirt with white print. (Sand L on backorder.)',
+		desc: 'Sand sweatshirt with white print (Sand L on backorder)',
 		images: [
 			'/assets/img/not-new-sand-hoody-w-landscape.JPG',
 			'/assets/img/not-new-sand-hoody-w-2.JPG',
@@ -46,7 +70,7 @@ var allProducts = [
 	},
 	{
 		title: 'White Sweatshirt',
-		desc: 'White sweatshirt with black print.',
+		desc: 'White sweatshirt with black print',
 		images: [
 			'/assets/img/not-new-white-hoody-m-3.JPG',
 			'/assets/img/not-new-white-hoody-m-1.JPG',
@@ -58,7 +82,7 @@ var allProducts = [
 	},
 	{
 		title: 'Red Sweatshirt',
-		desc: 'Red sweatshirt with white print. (Red L on backorder)',
+		desc: 'Red sweatshirt with white print',
 		images: [
 			'/assets/img/not-new-red-hoody-m-1.JPG',
 			'/assets/img/not-new-red-hoody-m-2.JPG',
@@ -71,7 +95,7 @@ var allProducts = [
 
 	{
 		title: 'Yellow Sweatshirt',
-		desc: 'Yellow sweatshirt with white print.',
+		desc: 'Yellow sweatshirt with white print',
 		images: [
 			'/assets/img/not-new-sweatshirt-yellow-1.JPG',
 			'/assets/img/not-new-sweatshirt-yellow-3.JPG',
--- _site/assets/js/product.js
+++ _site/assets/js/product.js
@@ -1,7 +1,19 @@
 var allProducts = [
 	{
+		title: 'Blue Sweatshirt',
+		desc: 'Blue sweatshirt with white and black print (Made to order)',
+		images: [
+			'/assets/img/not-new-blue-0003.jpg',
+			'/assets/img/not-new-blue-0004.jpg',
+			'/assets/img/not-new-blue-0002.jpg',
+			'/assets/img/not-new-blue-0001.jpg',
+		],
+		price: '45',
+		sku: 'NNH008'
+	},
+	{
 		title: 'White T-Shirt',
-		desc: 'White t-shirt with front and back black print.',
+		desc: 'White t-shirt with front and back black print',
 		images: [
 			'/assets/img/not-new-white-t-5.jpg',
 			'/assets/img/not-new-white-t-10.jpg',
@@ -9,11 +21,23 @@ var allProducts = [
 			'/assets/img/not-new-white-t-1.jpg',
 		],
 		price: '21',
+		sku: 'NNH004'
+	},
+	{
+		title: 'White T-Shirt',
+		desc: 'White t-shirt two-toned',
+		images: [
+			'/assets/img/not-new-two-colors-white-t-1.jpg',
+			'/assets/img/not-new-two-colors-white-t-2.jpg',
+			'/assets/img/not-new-two-colors-white-t-3.jpg',
+			'/assets/img/not-new-two-colors-white-t-4.jpg',
+		],
+		price: '29',
 		sku: 'NNH007'
 	},
 	{
 		title: 'Pink Sweatshirt',
-		desc: 'Pink sweatshirt with white print. (Pink L and XL on backorder.)',
+		desc: 'Pink sweatshirt with white print. (Pink L and XL on backorder)',
 		images: [
 			'/assets/img/not-new-pink-hoody-w-1.JPG',
 			'/assets/img/not-new-pink-hoody-w-2.JPG',
@@ -25,7 +49,7 @@ var allProducts = [
 	},
 	{
 		title: 'Sand Sweatshirt',
-		desc: 'Sand sweatshirt with white print. (Sand L on backorder.)',
+		desc: 'Sand sweatshirt with white print (Sand L on backorder)',
 		images: [
 			'/assets/img/not-new-sand-hoody-w-landscape.JPG',
 			'/assets/img/not-new-sand-hoody-w-2.JPG',
@@ -37,7 +61,7 @@ var allProducts = [
 	},
 	{
 		title: 'White Sweatshirt',
-		desc: 'White sweatshirt with black print.',
+		desc: 'White sweatshirt with black print',
 		images: [
 			'/assets/img/not-new-white-hoody-m-3.JPG',
 			'/assets/img/not-new-white-hoody-m-1.JPG',
@@ -49,7 +73,7 @@ var allProducts = [
 	},
 	{
 		title: 'Red Sweatshirt',
-		desc: 'Red sweatshirt with white print. (Red L on backorder)',
+		desc: 'Red sweatshirt with white print',
 		images: [
 			'/assets/img/not-new-red-hoody-m-1.JPG',
 			'/assets/img/not-new-red-hoody-m-2.JPG',
@@ -62,7 +86,7 @@ var allProducts = [
 
 	{
 		title: 'Yellow Sweatshirt',
-		desc: 'Yellow sweatshirt with white print.',
+		desc: 'Yellow sweatshirt with white print',
 		images: [
 			'/assets/img/not-new-sweatshirt-yellow-1.JPG',
 			'/assets/img/not-new-sweatshirt-yellow-3.JPG',
